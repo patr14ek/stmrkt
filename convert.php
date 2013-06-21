4e64f97bf5150fb2994e444810f50df024d90bd6
Lizz Uchambrina <lizon@ito-mhpi.ru>
2013-06-21 21:24:44 +0400

    added regexpes, change output

--- convert.php
+++ convert.php
@@ -7,6 +7,10 @@
  * To change this template use File | Settings | File Templates.
  */
 
+$comments_pattern = '/(\/\*).*?(\*\/)|(\/\/).*?(\n)|(<!--).*?(-->)/s';
+$language_pattern = '/[а-яА-ЯёЁ]/s';
+
+
 //используем функцию для определения кодировки
 function detectEncoding( $string )
 {
@@ -24,48 +28,41 @@ function detectEncoding( $string )
 function changeEncoding( $path )
 {
 
-    $flag = 0;
-    $amount = 0;
     foreach ( glob( $path . '/*.php' ) as $file ){
+
+        echo "  $file ... ";
 
         $content = file_get_contents( $file );
 
         //выбираем те файлы, которые не utf-8
         if ( detectEncoding( $content ) == 'windows-1251' ){
 
-            $lexem = token_get_all( $content );
-            foreach ( $lexem as $sType => $sString ){
+            global $comments_pattern;
+            global $language_pattern;
 
-                if ( $sType == T_INLINE_HTML ){
+            // убираем все комментарии из контента и записываем в новую переменную
+            $content_without_comments = preg_replace( $comments_pattern, "\n", $content );
 
+            // проверяем наличие русских букв в контенте без комментов. если они есть, то возвращается 1, если нет - 0
+            if( preg_match( $language_pattern, $content_without_comments ) == 0 ) {
 
-                    //если есть русские символы, выходим из цикла
-                    if ( preg_match( '/[А-Яа-я]/', $sType ) ){
+                $content = iconv( 'windows-1251', 'UTF-8', $content );
+                file_put_contents( $file, $content );
+                echo "[win1251] converted\n";
 
-                        $flag = true;
-                        break;
-                    }
+            } else {
 
-                    if ( $flag ){
-
-                        continue;
-                    } else{
-
-                        $content = iconv( 'windows-1251', 'UTF-8', $content );
-                        file_put_contents( $file, $content );
-                        $amount++;
-                        echo "File " . $file . " converted" . "\n";
-                    }
-                }
-
+                echo "[win1251] passed\n";
+                break;
             }
 
+        }
+        else {
+            echo "[UTF-8] passed\n";
         }
         global $files_count;
         $files_count++;
     }
-
-    echo $amount;
 
 }
 
@@ -82,9 +79,7 @@ function recursiveSearch( $path )
         // какая-то наркомания, но на php.net написано, что надо так перебирать файлы
         if ( is_dir( $test_file ) ){ // если папка, то выполнить эту же функцию в этой папке
             if ( $entry != ".." && $entry != '.' ){ // выводятся ещё и "." с "..", их игнорить
-                echo "\n";
                 echo "$test_file\n";
-                echo "-----------\n";
                 recursiveSearch( $test_file );
                 global $folders_count;
                 $folders_count++;
@@ -104,5 +99,3 @@ recursiveSearch( '.' );
 echo "\n";
 echo "Files passed: $files_count\n";
 echo "Folders passed: $folders_count\n";
-
-
